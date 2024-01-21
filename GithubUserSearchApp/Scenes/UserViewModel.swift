7650be6e2dc641a6bea26528e8086724816d7f9b
Abdullah NUR <abdullahnur.ybs@gmail.com>
2024-01-21 13:07:48 +0300

    Refactor: Date formatter added

--- GithubUserSearchApp/Scenes/UserViewModel.swift
+++ GithubUserSearchApp/Scenes/UserViewModel.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class UserViewModel {
     private var user: User?
-
+    
     func getUserInfo(username: String, completion: @escaping (Result<User, Error>) -> Void) {
         ApiService.shared.getUserInfo(username: username) { [weak self] result in
             switch result {
@@ -21,26 +21,37 @@ class UserViewModel {
             }
         }
     }
-
+    
+    
+    
     var username: String {
         return user?.login ?? ""
     }
     var publicRepoCount: Int {
         return user?.public_repos ?? 0
     }
-
+    
     var createdAt: String {
-        return user?.created_at ?? ""
+        if let rawDate = user?.created_at {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+            
+            if let date = dateFormatter.date(from: rawDate) {
+                dateFormatter.dateFormat = "dd MMMM yyyy"
+                return dateFormatter.string(from: date)
+            }
+        }
+        return ""
     }
-
+    
     var location: String {
         return user?.location ?? ""
     }
-
+    
     var avatarURL: URL? {
         return URL(string: user?.avatar_url ?? "")
     }
     
     
 }
-    
+
